f31ca984578756749ae026e2fd9733c195db04d2
Nap Joseph Calub <njncalub@gmail.com>
2021-07-06 16:34:39 +0800

    feat(calub-web): add public files

--- apps/calub/web/src/components/document/document-head.component.tsx
+++ apps/calub/web/src/components/document/document-head.component.tsx
@@ -10,16 +10,21 @@ const DocumentHead: React.FC<DocumentHeadProps> = (_props) => {
     <>
       <Head>
         <meta charSet="utf-8" />
-        <title>calub-web</title>
+        <title>Nap Joseph Calub</title>
 
-        <meta name="description" content="starter template for nextjs" />
+        <meta name="description" content="Curriculum Vitae of Nap Joseph Calub." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+        {/* See https://developers.google.com/web/fundamentals/web-app-manifest/ */}
         <link rel="manifest" href="/manifest.json" />
+
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+        <link rel="author" href="/humans.txt" />
+
         <meta name="theme-color" content="#ffffff" />
       </Head>
     </>
