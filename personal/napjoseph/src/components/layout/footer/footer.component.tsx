63fcf8e06496a8a78d12a75d0edfc0029f0c76a4
Nap Joseph Calub <njncalub@gmail.com>
2021-07-18 19:17:09 +0800

    style(napjoseph): remove included border

--- personal/napjoseph/src/components/layout/footer/footer.component.tsx
+++ personal/napjoseph/src/components/layout/footer/footer.component.tsx
@@ -38,20 +38,18 @@ const Footer: React.FC<FooterProps> = (_props) => {
         Footer
       </h2>
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-        <div className="mt-12 border-t border-gray-200 pt-8">
-          <p className="text-base text-gray-400 text-center">
-            &copy; {year}{' '}
-            <Link href="/">
-              <a className={classes.name}>{name}</a>
-            </Link>
-            . All rights reserved.
-          </p>
-          {socialLinks && (
-            <div className="my-4 flex justify-center space-x-6">
-              <SocialLinks socialLinks={socialLinks} />
-            </div>
-          )}
-        </div>
+        <p className="text-base text-gray-400 text-center">
+          &copy; {year}{' '}
+          <Link href="/">
+            <a className={classes.name}>{name}</a>
+          </Link>
+          . All rights reserved.
+        </p>
+        {socialLinks && (
+          <div className="my-4 flex justify-center space-x-6">
+            <SocialLinks socialLinks={socialLinks} />
+          </div>
+        )}
       </div>
     </footer>
   );
