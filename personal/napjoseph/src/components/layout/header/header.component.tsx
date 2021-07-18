456dede5dbe755fd73dcfb0dc984a1421f96c2c0
Nap Joseph Calub <njncalub@gmail.com>
2021-07-18 18:22:46 +0800

    feat(napjoseph): use cleaner header

--- personal/napjoseph/src/components/layout/header/header.component.tsx
+++ personal/napjoseph/src/components/layout/header/header.component.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import Image from 'next/image';
-import siteLogo from '../../../../public/logo.png';
+import Link from 'next/link';
 
 import classes from './header.module.scss';
 
@@ -9,60 +8,22 @@ import classes from './header.module.scss';
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = (_props) => {
-  const navigation = [];
-  const actions = [];
+  const title = 'Nap Joseph Calub';
+  const description = 'a byte-sized grimoire on software black magic & open-sourcery';
 
   return (
     <header className={classes.container}>
-      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
-        <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
-          <div className="flex items-center">
-            <a href="/">
-              <Image
-                className="h-2 w-auto"
-                src={siteLogo}
-                alt="Nap Joseph Calub Logo"
-                height={40}
-                width={40}
-              />
-              <span className="sr-only">Nap Joseph Calub</span>
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:flex lg:justify-between">
+        <div className="max-w-xl">
+          <Link href="/">
+            <a className={classes.title}>
+              <h1>{title}</h1>
             </a>
-            <div className="hidden ml-10 space-x-8 lg:block">
-              {navigation.map((link) => (
-                <a
-                  key={link.text}
-                  href={link.href}
-                  className="text-base font-medium text-white hover:text-indigo-50"
-                >
-                  {link.text}
-                </a>
-              ))}
-            </div>
-          </div>
-          <div className={classes.actions}>
-            {actions.map((link) => (
-              <a
-                key={link.text}
-                href={link.href}
-                className={link.is === 'main' ? classes.buttonMain : classes.buttonDefault}
-              >
-                {link.text}
-              </a>
-            ))}
-          </div>
+          </Link>
+
+          <p className={classes.description}>{description}</p>
         </div>
-        <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
-          {navigation.map((link) => (
-            <a
-              key={link.text}
-              href={link.href}
-              className="text-base font-medium text-white hover:text-indigo-50"
-            >
-              {link.text}
-            </a>
-          ))}
-        </div>
-      </nav>
+      </div>
     </header>
   );
 };
