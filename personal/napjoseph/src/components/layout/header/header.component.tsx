765cd48f43572a0494060b893bed53aa33dae92b
Nap Joseph Calub <njncalub@gmail.com>
2021-07-20 15:20:25 +0800

    feat(napjoseph): add animated logo

--- personal/napjoseph/src/components/layout/header/header.component.tsx
+++ personal/napjoseph/src/components/layout/header/header.component.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 import SiteNavigation from '../site-navigation/site-navigation.component';
 import { SITE_CONFIG } from '../../../config';
 
 import classes from './header.module.scss';
+import SiteLogo from '../../assets/site-logo.component';
 
 /* eslint-disable-next-line */
 interface HeaderProps {}
@@ -19,11 +21,44 @@ const Header: React.FC<HeaderProps> = (_props) => {
     <header className={classes.container}>
       <div className={classes.content}>
         <div className={classes.inner}>
-          <Link href="/">
-            <a className={classes.title}>
-              <h1>{title}</h1>
-            </a>
-          </Link>
+          <div className={classes.branding}>
+            <Link href="/">
+              <a className={classes.logo}>
+                <motion.div
+                  initial="hidden"
+                  animate="visible"
+                  whileHover="hover"
+                  variants={{
+                    hidden: {
+                      rotate: 0,
+                      opacity: 0
+                    },
+                    visible: {
+                      rotate: 180,
+                      opacity: 1,
+                      transition: {
+                        duration: 0.3
+                      }
+                    },
+                    hover: {
+                      rotate: 0,
+                      scale: 1.2,
+                      transition: {
+                        duration: 0.3
+                      }
+                    }
+                  }}
+                >
+                  <SiteLogo height={'50'} width={'50'} fill={'#1d4ed8'} />
+                </motion.div>
+              </a>
+            </Link>
+            <Link href="/">
+              <a className={classes.title}>
+                <h1 className={classes.value}>{title}</h1>
+              </a>
+            </Link>
+          </div>
 
           {description !== '' && <p className={classes.description}>{description}</p>}
 
