f2c8da9260cf5e03c297ea36e5c48b60858324c0
Nap Joseph Calub <njncalub@gmail.com>
2021-07-26 11:52:10 +0800

    style(napjoseph): use chakra app-wide

--- personal/napjoseph/src/components/articles/article-body.component.tsx
+++ personal/napjoseph/src/components/articles/article-body.component.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
 import { MDXRemote } from 'next-mdx-remote';
 
 import { Article } from '../../models';
@@ -14,14 +13,10 @@ interface ArticleBodyProps {
 const ArticleBody: React.FC<ArticleBodyProps> = ({ article }) => {
   if (!article) return null;
 
-  // NOTE: For now, only use chakra-ui in the article.
-
   return (
-    <ChakraProvider resetCSS={false}>
-      <div className={classes.container}>
-        <MDXRemote {...article.serializedContent} components={CustomComponents} />
-      </div>
-    </ChakraProvider>
+    <div className={classes.container}>
+      <MDXRemote {...article.serializedContent} components={CustomComponents} />
+    </div>
   );
 };
 
--- personal/napjoseph/src/pages/_app.page.tsx
+++ personal/napjoseph/src/pages/_app.page.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-
+import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 
 import Layout from '../components/layout/layout.component';
+import { theme } from '../theme/app';
 
 import '../vendor/font-awesome';
 import '../styles/tailwind.scss';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
   );
 };
 
