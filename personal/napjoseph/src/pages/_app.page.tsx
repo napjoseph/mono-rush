f5ef2e291dd8148f32b883d770d768ebb2849d92
Nap Joseph Calub <njncalub@gmail.com>
2021-07-29 15:16:35 +0800

    feat(napjoseph): add analytics using gtag

--- personal/napjoseph/src/pages/_app.page.tsx
+++ personal/napjoseph/src/pages/_app.page.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
+import * as gtag from '../lib/gtag';
 import Layout from '../components/layout/layout.component';
 import { theme } from '../theme/app';
 
 import '../vendor/font-awesome';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      // Add other events via `gtag.event`.
+      // For more details, check out:
+      // https://github.com/vercel/next.js/blob/master/examples/with-google-analytics/pages/contact.js
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ChakraProvider theme={theme}>
       <Layout>
