2a3c768b6a73fb4eb43bfe7ba41d4b10be62680b
Nap Joseph Calub <njncalub@gmail.com>
2021-08-09 14:07:23 +0800

    feat(calub-web): add gtag to page

--- personal/calub/src/pages/_document.page.tsx
+++ personal/calub/src/pages/_document.page.tsx
@@ -1,12 +1,28 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 
+import { GA_TRACKING_ID } from '../utils/gtag';
 import joinClassNames from '../utils/join-class-names';
 
 class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+            }}
+          />
+        </Head>
         <body className={joinClassNames('bg-gray-700', 'print:bg-white', 'with-scrollbars')}>
           <div>
             <Main />
